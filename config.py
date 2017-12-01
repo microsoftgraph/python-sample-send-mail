c1b8edd3234231098ca2e96c78ffa5562409e6c1
Doug Mahugh <dmahugh@microsoft.com>
2017-12-01 15:22:31 -0800

    update config.py string placeholders

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ In a production deployment, this information should be saved in a database or
 other secure storage mechanism.
 """
 
-CLIENT_ID = 'PUT YOUR CLIENT ID HERE'
-CLIENT_SECRET = 'PUT YOUR CLIENT SECRET HERE'
+CLIENT_ID = 'ENTER_YOUR_CLIENT_ID'
+CLIENT_SECRET = 'ENTER_YOUR_SECRET'
 REDIRECT_URI = 'http://localhost:5000/login/authorized'
 
 AUTHORITY_URL = 'https://login.microsoftonline.com/common'
@@ -14,7 +14,8 @@ TOKEN_ENDPOINT = '/oauth2/v2.0/token'
 
 RESOURCE = 'https://graph.microsoft.com/'
 API_VERSION = 'v1.0'
-SCOPES = ['User.Read', 'Mail.Send']
+SCOPES = ['User.Read'] # Add other scopes/permissions as needed.
+
 
 # This code can be removed after configuring CLIENT_ID and CLIENT_SECRET above.
 if ' ' in CLIENT_ID or ' ' in CLIENT_SECRET:
